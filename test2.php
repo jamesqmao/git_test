9a4b60c5289f48ef456da257d4f31c5077197b10
jamesqmao <qinglianmaoequal@gmail.com>
2012-10-12 16:16:18 -0700

    add new dr3

--- test2.php
+++ test2.php
@@ -2,6 +2,10 @@
 
 echo 'this is atest';
 
-echo 'this is atest2';
+echo 'this is atest2'; echo 'this is atest3';
+
+echo 'this is atest2'; echo 'this is atest3';
+
+echo 'this is atest2'; echo 'this is atest3';
 
 ?>
